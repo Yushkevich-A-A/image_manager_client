7c174a4641eac2fbe8c1f4b7637b86c352c469ca
Yushkevich-A-A <Mortifer1993@yandex.ru>
2021-07-24 19:53:53 +0300

    add fronend

---
+++ src/js/component/CloudServise/CloudServise.js
@@ -0,0 +1,8 @@
+import './DrawWidget/DrawWidget.css'
+import DrawWidget from "./DrawWidget/DrawWidget";
+import WidgetController from '../WidgetController/WidgetController';
+
+
+const widget = new DrawWidget();
+const controller = new WidgetController();
+
